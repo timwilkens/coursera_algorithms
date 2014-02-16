919934f8f53eb8ff71b0e2af3dd59e2c230511d8
Tim Wilkens <timwilkensf1@gmail.com>
2014-02-15 21:11:13 -0800

    Some lecture notes and script for selection sort.

---
+++ Week_2/selection_sort.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use List::Util qw(shuffle);
+
+my @nums = shuffle(1 .. 10);
+print "@nums\n";
+selection_sort(\@nums);
+print "@nums\n";
+
+
+sub selection_sort {
+  my $nums = shift;
+  my $size = scalar @nums;
+
+  for my $index (0 .. ($size - 2)) {  # don't need to look at the last element
+    my $min = $index;  # initialize min to our current index
+    for (my $j = ($index + 1); $j < $size; $j++) {   # look right from our current point
+      if ($nums->[$j] < $nums->[$min]) {
+        $min = $j;   # change index of minimum element
+      }
+    }
+    swap($nums,$index,$min);
+  }
+}
+
+sub swap {
+  my ($nums, $i, $j) = @_;
+
+  my $temp = $nums->[$i];
+  $nums->[$i] = $nums->[$j];
+  $nums->[$j] = $temp;
+}
