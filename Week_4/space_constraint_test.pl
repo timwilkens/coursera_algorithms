5c7f99b53c68852e66e7a61212b41e20935a26f9
Tim Wilkens <timwilkensf1@gmail.com>
2014-02-24 19:01:40 -0800

    Updates to better test linear vs. priority queue.

--- Week_4/space_constraint_test.pl
+++ Week_4/space_constraint_test.pl
@@ -24,19 +24,53 @@ use Data::Dumper;
 #              (warning: too few iterations for a reliable count)
 #  Linear: 282 wallclock secs (280.63 usr +  0.29 sys = 280.92 CPU) @  0.01/s (n=2)
 #
-#    Reasoning: linear search will M*N compares
+#    Reasoning: linear search will do M*N compares
 #                priority queue will do N(1 + log(M)) compares => N + N log M
 #
-#          When M > N log M => use priority queue , else use linear search
-###############################################################################################################
+#          When M*N > N + N log M => use priority queue , else use linear search
+#
+#           But, this simplifies to M > 1 + log(M)   which will always be true
+#         Conclusion then it should be that linear will never beat priority queue
+#           Perhaps an artifact of using Perl's objects to do the priority queue rather than direct array access
+#
+#          It seems to be the case that once N gets large (> 10000) priority queues basically always win
+###################################################################################################################
+
+my $M;
+my $N = 10000;
+
+my %data;
+
+for (my $i = 1000; $i < 1500; $i += 10000) {
+  $M = $i;
+  print "Starting test on $M\n";
+  my $results = timethese (
+    5,
+    {
+  #   'Dijkstra' => '&One',
+     'Binary_Heap' => '&Two',
+     'Linear' => '&Three'
+    },
+     'none'
+  );
+
+  $data{$M}{B} = $results->{Binary_Heap}[0];
+  $data{$M}{L} = $results->{Linear}[0];
+}
+
+print "VALUE\tBINARY\tLINEAR\n";
+for my $m_value (sort { $a <=> $b } keys %data) {
+  print join("\t", $m_value, $data{$M}{B}, $data{$M}{L}); 
+  print "\n";
+}
 
 sub One {
   my @nums;
 
-  for (shuffle(1 .. 1000000)) {
+  for (shuffle(1 .. $N)) {
     push @nums, $_;
     DijkstraThree::quicksort(\@nums, 0, (scalar(@nums) - 1));
-    if (scalar(@nums) > 1000) {
+    if (scalar(@nums) > $M) {
       pop @nums;
     }
   }
@@ -44,22 +78,21 @@ sub One {
 
 sub Two {
   my $pq = PQ->new();
-  for (shuffle(1 .. 1000000)) {
+  for (shuffle(1 .. $N)) {
     $pq->insert($_);
-    if ($pq->size > 1000) {
+    if ($pq->size > $M) {
       $pq->delete_max();
     }
   }
-  print "heap\n";
 }
 
 sub Three {
   my @nums;
 
-  for (shuffle(1 .. 1000000)) {
+  for (shuffle(1 .. $N)) {
     push @nums, $_;
 
-    if (scalar(@nums) > 1000) {
+    if (scalar(@nums) > $M) {
       my $max = 0;
       for my $i (1 .. scalar(@nums) - 1) {
         if ($nums[$i] > $nums[$max]) {
@@ -69,14 +102,5 @@ sub Three {
       splice(@nums, $max, 1);
     }
   }
-  print "linear\n";
 }
 
-timethese (
-  2,
-  {
-   'Dijkstra' => '&One',
-   'Binary Heap' => '&Two',
-   'Linear' => '&Three'
-  }
-);
